51bd2597f37293af963b3390fe21f1d7e7542276
venkat kishore <19490714+kittu401@users.noreply.github.com>
2019-08-16 12:59:33 +0530

    Delete Search.py

--- Search.py
+++
@@ -1,7 +0,0 @@
-def search(name2):
-    import re# function for checking wether given input present in that file or not
-    search_name = input("Enter name to be searched :")
-    f = open('%s.txt' % name2)
-    for line in f:
-        if re.search(search_name, line):
-            print(line, end=' ')
